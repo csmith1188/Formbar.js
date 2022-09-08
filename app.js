fc2b3ed7731a70965c53f95416657ff784dc14fb
Eshimmel <90261234+Eshimmel@users.noreply.github.com>
2022-09-08 11:20:26 -0400

    Incomplete

--- app.js
+++ app.js
@@ -37,20 +37,7 @@ app.get('/', (req, res) => {
 app.get('/login', (req, res) => {
     res.render('pages/login', {
         title: 'Formbar',
-        color: 'purple',
-        main: `
-            <button id="logInButton" class="pressed" onclick="changeUserType(this);">Log in</button>
-            <button id="newAccountButton" onclick="changeUserType(this);">New account</button>
-            <button id="guestButton" onclick="changeUserType(this);">Use as guest</button>
-            <form action="login" method="post" style="margin-bottom: 16px;">
-            <input type="text" id="usernameBox" name="username" class="box" placeholder="Name" value="" autocomplete="off" required>
-            <input type="password" id="passwordBox" name="password" class="box" placeholder="Password" value="" required>
-            <a href="/changepassword" id="forgotPassword">Forgot password</a>
-            <input type="hidden" id="userTypeBox" name="userType" value="login">
-            <input type="hidden" id="forwardBox" name="forward">
-            <input type="hidden" id="botBox" name="bot" value="False">
-            <input type="submit" id="submitButton" class="button unselectable" value="Log in" onsubmit="return false;">
-            </form>`
+        color: 'purple'
     });
 });
 
@@ -58,9 +45,6 @@ app.post('/login', (req, res) => {
     var user = { 
         username: req.body.username,
         password: req.body.password,
-        userType: req.body.userType,
-        forward: req.body.forward,
-        bot: req.body.bot
     }
     console.log(user.username + ' ' + user.userType);
     var passwordCrypt = encrypt(user.password);
