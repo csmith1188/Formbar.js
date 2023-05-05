7095fe01011a21db34856be2d03be42f322ade49
Eshimmel <90261234+Eshimmel@users.noreply.github.com>
2023-05-05 13:50:55 -0400

    Formbot Rewrite
    Flask server is up
    Working on adding authentication

--- app.js
+++ app.js
@@ -235,7 +235,7 @@ function joinClass(userName, code) {
 
 // Oauth2 Access Token Generator
 function generateAccessToken(username) {
-    return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
+    return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '10s' });
   }
 
 // Endpoints
@@ -875,9 +875,10 @@ app.post('/oauth/login', (req, res) => {
                 // Decrypt users password
                 let tempPassword = decrypt(JSON.parse(rows.password));
                 if (rows.username == user.username && tempPassword == user.password) {
-                    let token = generateAccessToken({ username: user.username })
-                    console.log(redurl + "?token=" + token);
-                    res.redirect(redurl + "?token=" + token)
+                    let token = generateAccessToken({ username: user.username, permissions: rows.permissions })
+                    res.set('authorization', token)
+                    // res.redirect(redurl + "?token=" + token)
+                    res.redirect(redurl)
                 } else {
                     res.redirect('/oauth/login?redurl=' + redurl)
                 }
