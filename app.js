b1563b96e2bdbea412404650aee942c80432b5af
Ryan24313 <ryan24313@live.ytech.edu>
2023-11-08 09:28:34 -0500

    fixed bug with id

--- app.js
+++ app.js
@@ -277,7 +277,7 @@ function joinClass(userName, code) {
 								console.log(classUser);
 								if (!classUser) {
 									db.run('INSERT INTO classusers(classuid, studentuid, permissions, digiPogs) VALUES(?, ?, ?, ?)',
-										[id.id, uid.id, GUEST_PERMISSIONS, 0], (err) => {
+										[classId.id, uid.id, GUEST_PERMISSIONS, 0], (err) => {
 											if (err) {
 												console.error(err)
 												return
