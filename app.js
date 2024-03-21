1d3a78c0544f928e0307f3971a14cab679a62c3f
Ryan24313 <ryan24313@live.ytech.edu>
2024-03-21 14:27:30 -0400

    fixed api 404 errors and reformatted api a bit

--- app.js
+++ app.js
@@ -1956,7 +1956,7 @@ app.use((req, res, next) => {
 
 		logger.log('warning', `[404] urlPath=(${urlPath}) ip=(${req.ip}) session=(${JSON.stringify(req.session)})`)
 
-		if (urlPath.startsWith('api')) {
+		if (urlPath.startsWith('api/')) {
 			res.status(404).json({ error: `The page ${urlPath} does not exist` })
 		} else {
 			res.status(404).render('pages/message', {
