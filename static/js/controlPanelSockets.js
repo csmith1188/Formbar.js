c3b86e9a52033964aafd8b8eb0d7473915a571f6
Ryan24313 <ryan24313@live.ytech.edu>
2024-04-29 08:43:13 -0400

    removed logs

--- static/js/controlPanelSockets.js
+++ static/js/controlPanelSockets.js
@@ -261,13 +261,11 @@ socket.on('customPollUpdate', (
 	let userPollsDiv = document.querySelector('div#userPolls')
 	let fastPollDiv = document.querySelector('div#quickPoll')
 
-	console.log(publicCustomPolls)
 	for (let i = 1; i <= 4; i++) {
 		let customPoll = customPolls[i]
 		let startButton = document.createElement('button')
 		startButton.className = 'start-custom-poll'
 		startButton.style.gridColumn = 3
-		console.log(customPolls)
 		startButton.textContent = customPoll.name
 		startButton.onclick = () => {
 			startPoll(i)
