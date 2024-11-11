c5f0cd9e9f4f7242e8f97f80479303ebcf4359ad
Parritz <masong1508@gmail.com>
2024-11-11 11:12:44 -0500

    Make template files get automatically copied

--- modules/config.js
+++ modules/config.js
@@ -1,7 +1,16 @@
 const fs = require("fs")
 
-// @TODO: Make it automatically copy the templates if they don't exist
 function getConfig() {
+    // If logNumber.json doesn't exist, create it
+    if (!fs.existsSync("logNumbers.json")) {
+        fs.copyFileSync("logNumbers-template.json", "logNumbers.json")
+    }
+
+    // If settings.json doesn't exist, create it
+    if (!fs.existsSync("settings.json")) {
+        fs.copyFileSync("settings-template.json", "settings.json")
+    }
+    
     return {
         logNumbers: JSON.parse(fs.readFileSync("logNumbers.json")),
         settings: JSON.parse(fs.readFileSync("settings.json"))
--- modules/database.js
+++ modules/database.js
@@ -1,6 +1,12 @@
 const sqlite3 = require("sqlite3")
+const fs = require("fs")
 
 function getDatabase() {
+    // If the database file doesn't exist, copy the template
+    if (!fs.existsSync("database/database.db")) {
+        fs.copyFileSync("database/database-template.db", "database/database.db")
+    }
+``
     // Establishes the connection to the database file
     return new sqlite3.Database("database/database.db")
 }
