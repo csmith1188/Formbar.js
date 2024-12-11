9143e68c8d9cc4da3364357cf767019ba6bba8f6
jesse26603 <jesse26603@live.ytech.edu>
2024-12-11 09:11:10 -0500

    Supplemental Commit
    Accidentally unchecked this change

--- modules/database.js
+++ modules/database.js
@@ -1,42 +1,42 @@
-const sqlite3 = require("sqlite3")
-const fs = require("fs")
+const sqlite3 = require("sqlite3");
+const fs = require("fs");
 const database = getDatabase();
 function getDatabase() {
     // If the database file doesn't exist, copy the template
     if (!fs.existsSync("database/database.db")) {
         fs.copyFileSync("database/database-template.db", "database/database.db")
-    }
-``
+    };
+
     // Establishes the connection to the database file
     return new sqlite3.Database("database/database.db")
-}
+};
 
 function getAll(query, params) {
 	return new Promise((resolve, reject) => {
 		database.all(query, params, (err, rows) => {
 			if (err) {
-                reject(new Error(err))
+                reject(new Error(err));
             } else {
-                resolve(rows)
-            }
-		})
-	})
-}
+                resolve(rows);
+            };
+		});
+	});
+};
 
 function runQuery(query, params) {
 	return new Promise((resolve, reject) => {
 		database.run(query, params, (err) => {
 			if (err) {
-                reject(new Error(err))
+                reject(new Error(err));
             } else {
-                resolve()
-            }
-		})
-	})
-}
+                resolve();
+            };
+		});
+	});
+};
 
 module.exports = {
     database,
     getAll,
     runQuery
-}
+};
