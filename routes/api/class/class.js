4d077777962c53033a78d7daac076a1757fdad3c
mason26611 <masong1508paypal@gmail.com>
2024-12-18 14:46:01 -0500

    Start using class ids rather than access codes

--- routes/api/class/class.js
+++ routes/api/class/class.js
@@ -5,45 +5,45 @@ const { logger } = require("../../../modules/logger")
 
 module.exports = {
     run(router) {
-        // Gets a class by key
-		router.get('/class/:key', async (req, res) => {
+        // Gets a class by id
+		router.get('/class/:id', async (req, res) => {
 			try {
-				// Get the class key from the request parameters
-				let key = req.params.key
+				// Get the class id from the request parameters
+				let classId = req.params.id;
 
 				// Log the request details
-				logger.log('info', `[get api/class/${key}] ip=(${req.ip}) session=(${JSON.stringify(req.session)})`)
+				logger.log('info', `[get api/class/${classId}] ip=(${req.ip}) session=(${JSON.stringify(req.session)})`);
 
 				// Get a clone of the class data
-				let classData = structuredClone(classInformation[key])
+				let classData = structuredClone(classInformation.classrooms[classId]);
 
 				// If the class does not exist, return an error
 				if (!classData) {
-					res.status(404).json({ error: 'Class not started' })
-					return
+					res.status(404).json({ error: 'Class not started' });
+					return;
 				}
 
 				// Get the poll responses in the class
-				classData.poll.responses = getPollResponses(classData)
+				classData.poll.responses = getPollResponses(classData);
 
 				// Get the user from the session
-				let user = req.session.user
+				let user = req.session.user;
 
 				// If the user is not in the class, return an error
 				if (!classData.students[user.username]) {
-					logger.log('verbose', `[get api/class/${key}] user is not logged in`)
-					res.status(403).json({ error: 'User is not logged into the selected class' })
-					return
+					logger.log('verbose', `[get api/class/${key}] user is not logged in`);
+					res.status(403).json({ error: 'User is not logged into the selected class' });
+					return;
 				}
 
 				// Get the users of the class
-				let classUsers = await getClassUsers(user, key)
+				let classUsers = await getClassUsers(user, key);
 
 				// If an error occurs, log the error and return the error
 				if (classUsers.error) {
-					logger.log('info', `[get api/class/${key}] ${classUsers}`)
-					res.status(404).json(classUsers)
-					return
+					logger.log('info', `[get api/class/${key}] ${classUsers}`);
+					res.status(404).json(classUsers);
+					return;
 				}
 
 				// If the user is not a teacher or manager, remove the poll history from the data
@@ -51,19 +51,22 @@ module.exports = {
 					delete classData.pollHistory;
 				}
 
+				// Remove the access code from the API
+				delete classData.key;
+
 				// Update the class data with the class users and remove the shared polls
-				classData.students = classUsers
-				delete classData.sharedPolls
+				classData.students = classUsers;
+				delete classData.sharedPolls;
 
 				// Log the class data
-				logger.log('verbose', `[get api/class/${key}] response=(${JSON.stringify(classData)})`)
+				logger.log('verbose', `[get api/class/${classId}] response=(${JSON.stringify(classData)})`);
 
 				// Send the class data as a JSON response
-				res.status(200).json(classData)
+				res.status(200).json(classData);
 			} catch (err) {
 				// If an error occurs, log the error and send an error message as a JSON response
-				logger.log('error', err.stack)
-				res.status(500).json({ error: 'There was a server error try again.' })
+				logger.log('error', err.stack);
+				res.status(500).json({ error: 'There was a server error try again.' });
 			}
 		})
     }
