a599cfb96dc244c4c88dde7a9e79d9aa897aaa07
mason26611 <masong1508paypal@gmail.com>
2025-01-30 16:22:19 -0500

    Fix login with Google button and database-template issue

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const session = require('express-session') // For storing client login data
 const crypto = require('crypto')
 const fs = require('fs')
 const authentication = require('./modules/authentication.js')
+const dotenv = require('dotenv')
 const { logger } = require('./modules/logger.js')
 const { MANAGER_PERMISSIONS, TEACHER_PERMISSIONS, GUEST_PERMISSIONS, STUDENT_PERMISSIONS, MOD_PERMISSIONS, BANNED_PERMISSIONS } = require('./modules/permissions.js')
 const { classInformation } = require('./modules/class.js')
@@ -11,6 +12,7 @@ const { database } = require('./modules/database.js')
 const { initSocketRoutes } = require('./sockets/init.js')
 const { app, io, http, getIpAccess } = require('./modules/webServer.js')
 const { upgradeDatabase } = require('./data_upgrader/dataFixer.js')
+dotenv.config()
 
 // Upgrade the database if it's not up to date
 upgradeDatabase();
