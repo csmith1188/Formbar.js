c11f8758bb3a594ff4c96c02a1c4acd7c80fb06b
Jesse <jesse26603@live.ytech.edu>
2025-02-11 09:35:32 -0500

    Oops

--- modules/googleOauth.js
+++ modules/googleOauth.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 function setupGooglePassport() {
     // If the .env file does not exist, skip
-    if (!fs.existsSync('.env') || !fs.readFileSync('.env').toString().includes('CLIENT_ID' || 'CLIENT_SECRET')) {
+    if (!fs.existsSync('.env') || !fs.readFileSync('.env').toString().includes('CLIENT_ID')) {
         return;
     };
 
