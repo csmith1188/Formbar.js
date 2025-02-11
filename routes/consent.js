0d836cf15c0308eb8157e6988dae6790cf969157
Jesse <jesse26603@live.ytech.edu>
2025-02-11 11:18:33 -0500

    Further Consent Page Additions

--- routes/consent.js
+++ routes/consent.js
@@ -7,12 +7,14 @@ module.exports = {
     run(app) {
         app.get('/consent', async (req, res) => {
             try {
-                // if (!req.query.app || !req.query.digipogs || !req.query.user || !req.query.redirectURL) {
+                // if (!req.query.app || !req.query.digipogs || !req.query.reciever || !req.query.user || !req.query.redirectURL) {
                 //     res.redirect('/');
                 //     return;
                 // }
+                // Set up the variables needed for the consent screen
                 req.session.redirect = req.query.redirectURL;
                 req.session.tempUser = req.query.user;
+                req.session.reciever = req.query.reciever;
                 const app =  req.query.app;
                 const digipogs = req.query.digipogs;
                 const appName = await new Promise((resolve, reject) => {
@@ -24,13 +26,12 @@ module.exports = {
                         };
                     });
                 });
-
                 logger.log('info', `[get /consent] ip=(${req.ip}) session=(${JSON.stringify(req.session)})`)
+                // Render the consent screen with the app name and the amount of digipogs being transferred
                 res.render('pages/consent', {
                     title: 'Consent',
                     name: appName, // The app requesting the transfer
                     digipogs: digipogs, // The amount of digipogs being transferred
-                    user: req.session.tempUser // The user requesting the transfer
                 });
             } catch (err) {
                 logger.log('error', err.stack);
@@ -42,9 +43,25 @@ module.exports = {
         });
         app.post('/consent', (req, res) => {
             try {
+                // Create the data object to be sent to the socket
+                const data = {
+                    digipogs: req.body.digipogs,
+                    reciever: req.session.reciever,
+                    user: req.session.tempUser
+                };
+                // Set the redirectURL constant and wipe the rest of the information used in consent from the session
+                const redirectURL = req.session.redirectURL;
+                req.session.redirectURL = undefined;
+                req.sesison.tempUser = undefined;
+                req.session.reciever = undefined;
+                // If the user accepts the transfer, emit the transferDigipogs event, otherwise emit the transferDenied event
                 if (req.body.consent === 'accept') {
-                    io.emit('transferDigipogs', req.body.digipogs);
-                }
+                    io.emit('transferDigipogs', data);
+                    res.redirect(redirectURL);
+                } else {
+                    io.emit('transferDenied', data);
+                    res.redirect(redirectURL);
+                };
             } catch (err) {
                 logger.log('error', err.stack);
                 res.render('pages/message', {
