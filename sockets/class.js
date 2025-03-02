021db7f1e93556c9bfb62e085eb3152a30e943c2
mason26611 <masong1508paypal@gmail.com>
2025-03-01 23:58:47 -0500

    Fix external app socket requests logging user out

--- sockets/class.js
+++ sockets/class.js
@@ -162,8 +162,7 @@ module.exports = {
                 for (const username in classInformation.users) {
                     const user = classInformation.users[username];
                     if (user.API == api) {
-                        // setClassOfApiSockets(api, user.activeClasses[0]);
-                        socket.emit('setClass', user.activeClasses[0]);
+                        setClassOfApiSockets(api, user.activeClasses[0]);
                         return;
                     }
                 }
