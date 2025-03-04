86a246fdc215e4a80adc2fc4a7ec43b94ca7ac99
mason26611 <masong1508paypal@gmail.com>
2025-03-04 12:00:28 -0800

    Database can now be downloaded by managers

--- routes/downloadDatabase.js
+++ routes/downloadDatabase.js
@@ -1,27 +1,20 @@
 const { isLoggedIn, permCheck } = require("../modules/authentication");
 const { logger } = require("../modules/logger");
-const { MANAGER_PERMISSIONS } = require("../modules/permissions");
+const {logNumbers} = require("../modules/config");
 
 module.exports = {
     run(app) {
-        app.post('/downloadDatabase', isLoggedIn, permCheck, (req, res) => {
+        app.get('/downloadDatabase', isLoggedIn, permCheck, (req, res) => {
 			try {
 				// Log the request details
-				logger.log('info', `[get api/downloadDatabase] ip=(${req.ip}) session=(${JSON.stringify(req.session)})`);
-
-				// Get the user from the session
-				const user = req.session.user;
-                if (user.permissions != MANAGER_PERMISSIONS) {
-                    res.status(403).json({ error: 'You do not have permission to perform this action.' });
-                    return;
-                }
-
+				logger.log('info', `[get /downloadDatabase] ip=(${req.ip}) session=(${JSON.stringify(req.session)})`);
                 res.download('database/database.db', 'database.db');
-				res.send("abc");
 			} catch (err) {
-				// If an error occurs, log the error and send an error message as a JSON response
 				logger.log('error', err.stack);
-				res.status(500).json({ error: 'There was a server error try again.' });
+				res.render('pages/message', {
+					message: `Error Number ${logNumbers.error}: There was a server error try again.`,
+					title: 'Error'
+				})
 			}
 		})
     }
