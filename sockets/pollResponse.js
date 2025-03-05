76fff23a59689f1888852f90cdec80bf7fb6b152
Jesse <jesse26603@live.ytech.edu>
2025-03-05 10:52:06 -0500

    Password Reset & .env-template Fix

--- sockets/pollResponse.js
+++ sockets/pollResponse.js
@@ -52,7 +52,6 @@ module.exports = {
                                 console.log(`Error adding ${amount} digipogs to ${username}`);
                                 console.error(err);
                             };
-                            console.log(`Added ${amount} digipogs to ${username}`);
                         });
                         earnedDigipogs[username] = username;
                     };
