cb9cff9c86893dd4da6f1385d009c27a1365e205
Jesse <jesse26603@live.ytech.edu>
2025-04-24 14:21:27 -0400

    Viewing Other Profiles
    The profile of any user can now be viewed by appending /# to the profile endpoint, where # is the id of the user

--- routes/profile.js
+++ routes/profile.js
@@ -5,29 +5,31 @@ const { logNumbers } = require('../modules/config');
 
 module.exports = {
     run(app) {
-        app.get('/profile', isVerified, async (req, res) => {
+        app.get('/profile/:userId?', isVerified, async (req, res) => {
             try {
                 // Log the request information
                 logger.log('info', `[get /profile] ip=(${req.ip}) session=(${JSON.stringify(req.session)})`);
-                const userData = await dbGet('SELECT * FROM users WHERE id = ?', [req.session.userId]);
+                const userId = req.params.userId || req.session.userId;
+                const userData = await dbGet('SELECT * FROM users WHERE id = ?', [userId]);
                 // Check if userData is null or undefined
                 if (!userData) {
                     logger.log('error', 'User data not found in database');
                     return res.render('pages/message', {
-                        message: `Error Number ${logNumbers.error}: There was a server error try again.`,
+                        message: 'Please enter a valid user ID.',
                         title: 'Error'
                     });
                 }
-                const { email, digipogs, API } = userData;
+                const { displayName, email, digipogs, API } = userData;
+                console.log(userData);
                 res.render('pages/profile', {
                     title: 'Profile',
-                    displayName: req.session.displayName,
-                    user: req.session.username,
+                    displayName: displayName,
                     email: email,
                     digipogs: digipogs,
-                    id: req.session.userId,
-                    API: API
+                    id: userId,
+                    API: req.session.userId == req.params.userId || req.params.userId == undefined ? API : null,
                 });
+                console.log(req.session.userId, req.params.userId, req.session.userId == req.params.userId);
             } catch (err) {
                 logger.log('error', err.stack);
                 res.render('pages/message', {
