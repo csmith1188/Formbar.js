ed2d3ee827fc274b73f447bb183419759c6fbea1
mason26611 <123019028+mason26611@users.noreply.github.com>
2025-04-25 08:44:41 -0400

    Fix class name update not updating on frontend

--- sockets/class.js
+++ sockets/class.js
@@ -250,6 +250,7 @@ module.exports = {
 
         socket.on('changeClassName', (name) => {
             try {
+                logger.log('info', `[changeClassName] ip=(${socket.handshake.address}) session=(${JSON.stringify(socket.request.session)})`)
                 if (!name) {
                     socket.emit('message', 'Class name cannot be empty.');
                     return;
@@ -261,7 +262,8 @@ module.exports = {
                         if (err) throw err;
 
                         // Update the class name in the class information
-                        classInformation.classrooms[socket.request.session.classId].name = name;
+                        classInformation.classrooms[socket.request.session.classId].className = name;
+                        socket.emit('changeClassName', name);
                         socket.emit('message', 'Class name updated.');
                     } catch (err) {
                         logger.log('error', err.stack);
