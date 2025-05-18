17eacde55f8b022207e1c4ae4895950784c03c5b
mason26611 <masong1508paypal@gmail.com>
2025-05-18 16:45:02 -0400

    remove console log

--- routes/login.js
+++ routes/login.js
@@ -30,7 +30,6 @@ module.exports = {
                     res.redirect('/');
                     return;
                 } else if (!token) {
-                    console.log(settings.googleOauthEnabled)
                     logger.log('info', `[get /login] ip=(${req.ip}) session=(${JSON.stringify(req.session)})`)
                     res.render('pages/login', {
                         title: 'Login',
