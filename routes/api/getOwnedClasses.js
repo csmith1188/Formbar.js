858a3d437f5205727d038a6b8033b22179ff22c3
Jesse <jesse26603@live.ytech.edu>
2025-09-03 14:10:41 -0400

    Removed User Page
    Because the Profile page serves the same function as the User page, the User page was removed.

--- routes/api/getOwnedClasses.js
+++ routes/api/getOwnedClasses.js
@@ -1,13 +1,13 @@
-const { logger } = require("../../../modules/logger")
-const { dbGet } = require("../../../modules/database");
-const { getUserOwnedClasses} = require("../../../modules/user");
-const { httpPermCheck } = require("../../middleware/permissionCheck");
-const { createSocketFromHttp } = require("../../../modules/webServer");
+const { logger } = require("../../modules/logger")
+const { dbGet } = require("../../modules/database");
+const { getUserOwnedClasses} = require("../../modules/user");
+const { httpPermCheck } = require("../middleware/permissionCheck");
+const { createSocketFromHttp } = require("../../modules/webServer");
 
 module.exports = {
     run(router) {
         // Gets a class by id
-        router.get('/user/:id/ownedClasses', httpPermCheck('getOwnedClasses'), async (req, res) => {
+        router.get('/profile/:id/ownedClasses', httpPermCheck('getOwnedClasses'), async (req, res) => {
             try {
                 const userId = req.params.id;
                 const user = await dbGet('SELECT * FROM users WHERE id = ?', [userId]);
