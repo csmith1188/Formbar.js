deeb6d40f75d34c96fa8beee945504f50edf8843
mason26611 <123019028+mason26611@users.noreply.github.com>
2025-10-07 22:18:26 -0400

    Add error handling and fix some issues

--- modules/config.js
+++ modules/config.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const crypto = require('crypto');
-const { dbGet } = require("./database");
+const { dbGet, dbRun } = require("./database");
 require('dotenv').config();
 
 /**
