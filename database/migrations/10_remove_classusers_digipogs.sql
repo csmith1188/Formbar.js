cf026e6811c86e935bcc1038d9c9500d88bb7b1c
Jesse <jesse26603@live.ytech.edu>
2025-10-20 10:07:54 -0400

    Update 10_remove_classusers_digipogs.sql

--- database/migrations/10_remove_classusers_digipogs.sql
+++ database/migrations/10_remove_classusers_digipogs.sql
@@ -2,4 +2,9 @@ CREATE TABLE IF NOT EXISTS temp_classusers (
     "classId"       INTEGER NOT NULL,
     "studentId"     INTEGER NOT NULL,
     "permissions"   INTEGER NOT NULL
-)
+);
+
+INSERT INTO temp_classusers ("classId", "studentId", "permissions")
+SELECT "classId", "studentId", "permissions" FROM classusers;
+DROP TABLE classusers;
+ALTER TABLE temp_classusers RENAME TO classusers;
