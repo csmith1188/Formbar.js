eb34b8e556bea868077dfdbd21c1c8a499762fdc
mason26611 <123019028+mason26611@users.noreply.github.com>
2025-10-27 06:46:35 -0400

    Fix issues

--- routes/changePassword.js
+++ routes/changePassword.js
@@ -22,7 +22,11 @@ module.exports = {
                 // Set session email so that it can be used when changing the password
                 // After that, get their token from the database
                 req.session.email = req.query.email;
-                const token = await dbGet("SELECT secret FROM users WHERE email = ?", [req.session.email]);
+                const userData = await dbGet("SELECT secret FROM users WHERE email = ?", [req.session.email]);
+                let token;
+                if (userData) {
+                    token = userData.secret;
+                }
 
                 // If the token is valid, render the page to let the user reset their password
                 // If not, render an error message
@@ -44,14 +48,15 @@ module.exports = {
 
         app.post("/changepassword", async (req, res) => {
             try {
-                const token = await dbGet("SELECT secret FROM users WHERE email = ?", [req.session.email || req.body.email]);
-                if (!token) {
+                const userData = await dbGet("SELECT secret FROM users WHERE email = ?", [req.session.email || req.body.email]);
+                if (!userData) {
                     return res.render("pages/message", {
                         message: "No user found with that email.",
                         title: "Error",
                     });
                 }
 
+                let token = userData.secret;
                 if (req.body.email) {
                     // Send an email to the user with the password change link
                     const location = `${req.protocol}://${req.get("host")}`;
