07cb74ba5630d4026a2132b0695700a709cebea2
mason26611 <masong1508paypal@gmail.com>
2025-11-05 10:17:35 -0500

    Combine redundant database migrations

--- database/migrations/06_remove_old_tables.sql
+++ database/migrations/06_remove_old_tables.sql
@@ -1,3 +1,6 @@
+-- 06_remove_old_tables.sql
+-- This migration removes obsolete tables: apps, lessons, plugins, and stats.
+
 DROP TABLE IF EXISTS apps;
 DROP TABLE IF EXISTS lessons;
 DROP TABLE IF EXISTS plugins;
