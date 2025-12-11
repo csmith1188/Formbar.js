c880697c1696925b0c45549bbe96cf5437d7c99a
mason26611 <masong1508paypal@gmail.com>
2025-12-11 14:53:18 -0500

    Fix pool payout dates

--- sockets/pools.js
+++ sockets/pools.js
@@ -173,7 +173,7 @@ module.exports = {
                             pool.id,
                             amountPerMember,
                             `Pool Payout`,
-                            new Date().toISOString(),
+                            new Date(),
                         ]);
                     }
                 }
