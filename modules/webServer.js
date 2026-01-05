984fd6622d81e6fe9149b3751c1d6d4d1c1ba515
mason26611 <masong1508paypal@gmail.com>
2026-01-05 10:48:14 -0500

    Add SwaggerDocs to project

--- modules/webServer.js
+++ modules/webServer.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
 const { dbGetAll } = require("./database");
 
 // Create the express server and attach socket.io to it
@@ -6,6 +8,23 @@ function createServer() {
     const app = express();
     const http = require("http").createServer(app);
     const io = require("socket.io")(http);
+    const swaggerDocOptions = {
+        definition: {
+            openapi: "3.0.0",
+            info: {
+                title: "Formbar API",
+                version: "3.0.0",
+                description: "HTTP API documentation for Formbar.js."
+            }
+        },
+        apis: [
+            "./api/v1/**/*.js",
+            "./docs/components/**/*.yaml"
+        ],
+    }
+
+    const specs = swaggerJsdoc(swaggerDocOptions);
+    app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs));
 
     return { app, io, http };
 }
