d7df7bbc7ad2956a5540a91f953dfdbf6046c554
hayde26904 <haydenfrobenius750@gmail.com>
2026-01-07 11:19:33 -0500

    separated logs route

---
+++ api/v1/controllers/logs.js
@@ -0,0 +1,29 @@
+const { permCheck, isAuthenticated } = require("@controllers/middleware/authentication");
+const {getAllLogs, getLog} = require('@services/log-service');
+const { logger } = require("@modules/logger");
+const fs = require("fs");
+
+module.exports = (router) => {
+    // Handle displaying all logs to the manager
+    router.get("/logs", async (req, res) => {
+        try {
+            logger.log("info", `[get /logs] ip=(${req.ip}) session=(${JSON.stringify(req.session)})`);
+            const logs = await getAllLogs();
+            res.json(logs);
+        } catch (err) {
+            throw new Error(`Failed to retrieve logs: ${err.message}`);
+        }
+    });
+
+    // Handle displaying a specific log to the manager
+    router.get("/logs/:log", async (req, res) => {
+        const logFileName = req.params.log;
+        try {
+            logger.log("info", `[get /logs/:log] ip=(${req.ip}) session=(${JSON.stringify(req.session)})`);
+            const content = await getLog(logFileName);
+            res.json(content);
+        } catch (err) {
+            throw new Error(`Failed to retrieve log file ${logFileName}: ${err.message}`);
+        }
+    });
+}
---
+++ api/v1/services/log-service.js
@@ -0,0 +1,36 @@
+const fs = require('fs').promises;
+const logDir = 'logs/';
+
+async function getAllLogs() {
+    try {
+        const files = await fs.readdir(logDir);
+        const logs = await Promise.all(files
+            .filter((fileName) => fileName.endsWith('.log'))
+            .map(async (fileName) => {
+            try {
+                const stat = await fs.stat(`${logDir}${fileName}`);
+                return stat.size > 0 ? fileName : null; // Exclude empty log files
+            } catch (e) {
+                return null;
+            }
+            })
+        );
+        return logs.filter(Boolean); // Remove null values
+    } catch (err) {
+        throw new Error(`Failed to retrieve logs: ${err.message}`);
+    }
+}
+
+async function getLog(logFileName) {
+    try {
+        const content = await fs.readFile(`${logDir}${logFileName}`, 'utf8');
+        return content;
+    } catch (err) {
+        throw new Error(`Failed to read log file ${logFileName}: ${err.message}`);
+    }
+}
+
+module.exports = {
+    getAllLogs,
+    getLog
+}
