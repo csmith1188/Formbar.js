7e7e472c674c4ae1a885f1f19efcd8b3ead23b2a
Levi Knudson <164372586+27lknudson@users.noreply.github.com>
2026-01-15 14:55:29 -0500

    Remove redundant student endpoint.

--- database/init.sql
+++ database/init.sql
@@ -127,7 +127,6 @@ CREATE TABLE IF NOT EXISTS "transactions"
 CREATE TABLE IF NOT EXISTS "users"
 (
     "id"          INTEGER NOT NULL UNIQUE,
-    "username"    TEXT    NOT NULL,
     "email"       TEXT    NOT NULL UNIQUE,
     "password"    TEXT,
     "permissions" INTEGER,
