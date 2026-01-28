58b50ab7b587ff58f0fcb5defcf97ad2e5c9422d
mason26611 <123019028+mason26611@users.noreply.github.com>
2026-01-28 14:57:15 -0500

    Begin OAuth API

---
+++ api/v1/controllers/oauth/authorize.js
@@ -0,0 +1,11 @@
+const { logger } = require("@modules/logger");
+const { classInformation } = require("@modules/class/classroom");
+const { Student } = require("@modules/student");
+const authService = require("@services/auth-service");
+const ValidationError = require("@errors/validation-error");
+
+module.exports = (router) => {
+    router.get("/oauth/authorize", async (req, res) => {
+        // @TODO
+    });
+};
