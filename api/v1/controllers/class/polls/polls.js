33a70227be69b8de38e6edf20eb6ffd863212451
Landon Harnish <28lharnish@live.ytech.edu>
2026-09-10 09:35:59 -0400

    Update polls.js

--- api/v1/controllers/class/polls/polls.js
+++ api/v1/controllers/class/polls/polls.js
@@ -298,7 +298,7 @@ module.exports = (router) => {
     router.get(
         "/class/:id/pollhistory",
         isAuthenticated,
-        isOwnerOrHasScopes(SCOPES.CLASS.SYSTEM.ADMIN, "You do not have permission to view polls with user responses for this class."),
+        isOwnerOrHasScopes(membershipService.classroomOwnerCheck, SCOPES.CLASS.SYSTEM.ADMIN, "You do not have permission to view polls with user responses for this class."),
         async (req, res) => {
             const classId = req.params.id;
             requireQueryParam(classId, "classId");
