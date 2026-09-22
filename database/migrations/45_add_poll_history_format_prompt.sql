032a704d5403ad7daa53ca31c903a71032a3dd05
28TMurray <28tmurray@live.ytech.edu>
2026-09-22 08:19:12 -0400

    Formatted Poll Prompt Migration Hotfix

--- database/migrations/45_add_poll_history_format_prompt.sql
+++ database/migrations/45_add_poll_history_format_prompt.sql
@@ -4,9 +4,3 @@
 -- will always run because migration 16 and/or 17 always modifies the poll_history table
 ALTER TABLE poll_history ADD COLUMN promptMD    TEXT;
 ALTER TABLE poll_history ADD COLUMN promptHTML  TEXT;
-
-UPDATE poll_history
-SET 
-    promptMD = prompt,
-    promptHTML = prompt
-WHERE promptMD IS NULL OR promptHTML IS NULL;
